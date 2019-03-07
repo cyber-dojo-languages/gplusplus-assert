5ac67dadb612d30839e14c711d885d81e598f65c
Jon Jagger <jon@jaggersoft.com>
2019-03-07 17:57:47 +0100

    simplify red/amber/green determination by using status==0 for green (thus avoiding need for specific green 'All tests passed' text)

--- docker/red_amber_green.rb
+++ docker/red_amber_green.rb
@@ -1,7 +1,7 @@
 
 lambda { |stdout,stderr,status|
   output = stdout + stderr
+  return :green if status == 0
   return :red   if /(.*)Assertion(.*)failed/.match(output)
-  return :green if /(All|\d+) tests passed/.match(output)
   return :amber
 }
